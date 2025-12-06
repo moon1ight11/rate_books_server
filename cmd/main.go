940f6c148ed30405dda8908da84d85dc39cf000c
Moon1ight <fedorchervak@gmail.com>
2025-12-06 14:45:48 +0700

    fix(main) : update architecture

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,1 @@
 package main
-
-import (
-	"rate_books/internal/database"
-	"rate_books/internal/handlers"
-)
-
-func main() {
-	// подключение к базе данных
-	database.Connection()
-	defer database.DB.Close()
-
-	// обновление миграций
-	database.UppingMigrations()
-
-	// роутер
-	handlers.Router()
-}
--- internal/model/user.go
+++
@@ -1,7 +0,0 @@
-package model
-
-// модель юзера
-type User struct {
-	UserName string `json:"user_name"`
-	Pass     string `json:"pass"`
-}
